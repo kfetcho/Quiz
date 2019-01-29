3d72f5e386587d00cf886090e93fecc79486ad08
Kyle Fetcho <kfetcho@i143>
2019-01-29 11:20:40 -0500

    Version 3: Fully functional quiz app meeting all requirements for parts 1 and 2 of the project including the loading screen, signature, and 5 additional questions.

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Kyle Fetcho on 1/23/19.
 //  Copyright © 2019 Kyle Fetcho. All rights reserved.
-//
+//  High Point University
 
 import UIKit
 
@@ -15,12 +15,23 @@ class ViewController: UIViewController {
     let questions: [String] = [
         "What is 7+7?",
         "What is the capital of Vermont?",
-        "What is cognac made from?"
+        "What is cognac made from?",
+        "When was High Point University founded?",
+        "Who is the current president of HPU?",
+        "What are High Points school colors?",
+        "What NFL team does Robert Kraft own?",
+        "Where is Super Bowl LIII going to be held?"
     ]
     let answers: [String] = [
         "14",
         "Montpelier",
-        "Grapes"
+        "Grapes",
+        "1924",
+        "Dr. Nido Qubein",
+        "Purple and white",
+        "The New England Patriots",
+        "Mercedes-Benz Stadium in Atlanta, GA"
+        
     ]
 
     var currentQuestionIndex: Int = 0
